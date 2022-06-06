5d54da240f263e293cf68a41f3a897342a282df9
Samira de Melo Oliveira <80074956+SamiraOliveira@users.noreply.github.com>
2022-06-06 10:59:25 -0300

    Add files via upload

---
+++ aula_06_06_22/exercicio.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Language', 'Speakers (in millions)'],
+          ['Fevereiro', 80],
+          ['Março', 30],
+          ['Abril', 7.02],
+          ['Maio', 25]
+        ]);
+
+      var options = {
+        legend: 'right',
+        pieSliceText: 'label',
+        title: 'Gráfico da minha frequência no ano',
+      };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="piechart" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>
